5d8edf08cefcf94321d2ab24aa7dfd5aefa9b431
Gevorg <narodniyplusmagazin@gmail.com>
2025-12-23 18:49:45 +0400

    fix: change base url

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-// export const API_BASE_URL = 'https://magazin-9614959e5831.herokuapp.com';
-export const API_BASE_URL = 'http://172.20.10.6:5050'
+export const API_BASE_URL = 'https://magazin-9614959e5831.herokuapp.com';
+// export const API_BASE_URL = 'http://172.20.10.6:5050'
